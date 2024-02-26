c338e3d011608dccf399f733468a0216b1ba453c
shu <darja0604@gmail.com>
2024-02-26 12:11:21 +0300

    Практическая работа с функциями. 27 вариант.

--- practicalWork_260224/main.js
+++ practicalWork_260224/main.js
@@ -8,24 +8,33 @@ let fact = [fact1 = 1, fact2 = 1, fact3 = 1]
 // let fact2 = 1;
 // let fact3 = 1;
 
-let numbers = (num1, num2, num3) => {
-    for (let i = 0; i < num1; i++) {
-        fact1 *= i;      
+let numbers = (array) => {
+    for (let i = 1; i <= array[0]; i++) {
+        fact[0] *= i; 
+           
     }
+    console.log(`Факториал числа 2 (первого числа списка) = ${fact[0]}`);
 
-    for (let i = 0; i < num2; i++) {
-        fact2 *= i;         
+    for (let i = 1; i <= array[1]; i++) {
+        fact[1] *= i;         
     }
+    console.log(`Факториал числа 4 (второго числа списка) = ${fact[1]}`);
 
-    for (let i = 0; i < num3; i++) {
-        fact3 *= i;            
+    for (let i = 1; i <= array[2]; i++) {
+        fact[2] *= i;            
     }
+    console.log(`Факториал числа 7 (третьего числа списка) = ${fact[2]}`);
 
-    for (let i = 0; i < numbers.length; i++) {
+    console.log("Заданное значение: 25");
+    console.log("Список чисел, содержащий только числа, факториал которых не превышает заданное значение:");
+
+    for (let i = 0; i < fact.length; i++) {
         if (fact[i] <= numConst) {
-            console.log();
+            console.log(array[i]);
         }
     }
     
 } 
-numbers(2, 4, 7);
+const nums = [2, 4, 7]
+numbers(nums);
+
