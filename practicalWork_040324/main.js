822764729629817837cbff99ebc2b46800ea3ef6
shu <darja0604@gmail.com>
2024-03-04 15:23:48 +0300

    Практическая работа от 04.03.24

---
+++ practicalWork_040324/main.js
@@ -0,0 +1,9 @@
+/*27.	Напишите функцию find_latest_movie(movies), которая находит фильм 
+с самым поздним годом выпуска в списке фильмов.*/
+
+console.log("Вариант 27.")
+
+let find_latest_movie = (nameMovie, Date) => {
+    let lastFilmDate = 0;
+    let lastFilmName = "";
+}
